e0ea8b11a6806a24afc6f5416d1fd5acf5e2b93a
jay gupta <jaygupta.210123@gmail.com>
2026-08-02 23:10:48 +0530

    Time: 24 ms (9.39%) | Memory: 31.9 MB (9.49%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -5,8 +5,8 @@ public:
         vector<int>ans;
         unordered_set<int>m;
         int a,b;
-        long long exptsum=0;
-        long long actualsum=0;
+        long exptsum;;
+        long actualsum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 actualsum+=grid[i][j];
