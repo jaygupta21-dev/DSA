7efaa95b4ec4d5362e71a42c31db62889af4a836
jay gupta <jaygupta.210123@gmail.com>
2026-08-05 21:27:53 +0530

    Time: 661 ms (21.84%) | Memory: 295.7 MB (23.05%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    void merge(vector<int>& arr, int st, int mid, int end) {
+
+        vector<int> temp;
+
+        int i = st;
+        int j = mid + 1;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp.push_back(arr[i]);
+                i++;
+            } else {
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+
+        while (i <= mid) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+
+        while (j <= end) {
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+        for (int k = 0; k <= temp.size()-1; k++) {
+            arr[k+st] = temp[k];
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int st, int end) {
+
+        if (st >= end)
+            return;
+
+        int mid = st + (end - st) / 2;
+
+        mergeSort(arr, st, mid);
+        mergeSort(arr, mid + 1, end);
+
+        merge(arr, st, mid, end);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+};
