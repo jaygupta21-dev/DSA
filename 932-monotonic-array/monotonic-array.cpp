8e06fb3c65d7dc42570da67a7be2980000c2a308
jay gupta <jaygupta.210123@gmail.com>
2026-08-06 00:15:24 +0530

    Time: 4 ms (21.35%) | Memory: 100.5 MB (6.76%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,30 +1,22 @@
 class Solution {
+    bool issorted(vector<int>&arr,int n){
+        if(n==0||n==1){
+        return true;
+      }
+      return arr[n-1]>=arr[n-2] && issorted(arr,n-1);
+
+    }
+     bool issorted2(vector<int>&arr,int n){
+        if(n==0||n==1){
+        return true;
+      }
+      return arr[n-1]<=arr[n-2] && issorted2(arr,n-1);
+     }
+
 public:
     bool isMonotonic(vector<int>& arr) {
         int n=arr.size();
-        int j=n-1;
-        int i=n-1;
-        int count_a=0;
-        int count_b=0;
-        while(j>0){
-            if(arr[j]>=arr[j-1]){
-                count_a++;
-            }
-            j--;
-        }
-        if(count_a==n-1){
-            return true;
-        }
-        while(i>0){
-            if(arr[i-1]>=arr[i]){
-                count_b++;
-            }
-            i--;
-        }
-        if(count_b==n-1){
-            return true;
-        }
-        return false;
+       return issorted(arr,n) || issorted2(arr,n);
         
     }
 };
